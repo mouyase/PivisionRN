36a9e9c3c91abb50caaa685949c5a5793437c25e
某亚瑟 <i@yojigen.tech>
2024-04-07 23:01:14 +0800

    chore: 使用expo的新架构

--- android/app/src/main/java/com/pivisionrn/MainActivity.java
+++ android/app/src/main/java/com/pivisionrn/MainActivity.java
@@ -1,17 +1,12 @@
 package com.pivisionrn;
+import expo.modules.ReactActivityDelegateWrapper;
 
-import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 public class MainActivity extends ReactActivity {
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
-  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -29,10 +24,10 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
-    return new DefaultReactActivityDelegate(
+    return new ReactActivityDelegateWrapper(this, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED, new DefaultReactActivityDelegate(
         this,
         getMainComponentName(),
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        DefaultNewArchitectureEntryPoint.getFabricEnabled());
+        DefaultNewArchitectureEntryPoint.getFabricEnabled()));
   }
 }
--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,3 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  env: {
-    production: {
-      plugins: ['transform-remove-console'],
-    },
-  },
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./'],
-        extensions: ['.js', '.ts', '.tsx', '.tsx'],
-        alias: {
-          '@': './src/',
-        },
-      },
-    ],
-  ],
+  presets: ['babel-preset-expo'],
 }
