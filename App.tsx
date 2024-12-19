0651b42e480d3c4e65375e55798384c5c4fd77bb
某亚瑟 <i@yojigen.tech>
2024-12-19 20:56:06 +0800

    feat: 移除文件

--- App.tsx
+++ App.tsx
@@ -1,25 +1,23 @@
 import { StatusBar } from 'expo-status-bar'
-import { getApiKey } from 'modules/expo-native-configuration'
-import { useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+
+import { getApiKey } from 'modules/expo-native-configuration'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>API key: {getApiKey()}</Text>
-      </View>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>API key: {getApiKey()}</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
     justifyContent: 'center',
   },
 })
