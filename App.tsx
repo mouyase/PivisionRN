925d5dde70a087cd3e5d8fc05e0f295a74c41803
某亚瑟 <mouyase@seaart-donghao.local>
2025-10-10 15:13:18 +0800

    feat: 增加命令

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import "./global.css"
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text className={'text-cyan-300'}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
