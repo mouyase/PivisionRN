63ef229cf23d6e2dd256b0720a9658a0a51e5dba
某亚瑟 <mouyase@seaart-donghao.local>
2025-10-10 14:56:45 +0800

    feat: 框架更新

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
-import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
-import './global.css';
+import { StyleSheet, Text, View } from 'react-native';
+import "./global.css"
 
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,8 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativewind } = require("nativewind/metro");
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = withNativewind(config);
